dbe2d637361ca189a24ca673dca069337302b419
Zandra Max <s333772@oslomet.no>
2020-04-20 17:20:55 +0200

    oppdatert side for kunder

--- src/main/java/org/datavelger/KundesideController.java
+++ src/main/java/org/datavelger/KundesideController.java
@@ -1,14 +1,27 @@
 package org.datavelger;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Popup;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
@@ -16,7 +29,8 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.ResourceBundle;
 
-public class KundesideController implements Initializable{
+public class KundesideController implements Initializable {
+    Stage window;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -29,11 +43,18 @@ public class KundesideController implements Initializable{
                 e.printStackTrace();
             }
         });
-
+        previousConf.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            try {
+                switchToPreviousConfig();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
         choose.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
             innerPane.getChildren().clear();
             addChosenComps(checkSelected());
         });
+
     }
 
     private Boolean[] checkSelected(){ //finner hvilke knapper som er trykket
@@ -59,8 +80,35 @@ public class KundesideController implements Initializable{
                 Label label = new Label(labelText);
                 label.setLayoutY(positionY);
                 nodes.add(label);
+                String lowerCase = labelText.toLowerCase();
+                System.out.println("Du har valgt "+lowerCase);
+                Button add = new Button("Velg "+lowerCase);
+                Button remove = new Button("Fjern valgt "+lowerCase);
+                nodes.add(add);
+                nodes.add(remove);
+                add.setLayoutX(150);
+                remove.setLayoutX(300);
+                add.setLayoutY(positionY);
+                remove.setLayoutY(positionY);
+                String finalLabelText = lowerCase;
 
-                ChoiceBox<String> brandBox = new ChoiceBox<>();
+                //Kaller på metoden i showChooser med navnet på knappen som er valgt
+                add.setOnAction(event -> showChooser.pressedButton(finalLabelText));
+
+                 /* System.out.println("Du har valgt grafikkort");
+                    Button graphics = new Button("Velg grafikkort");
+                    graphics.setOnAction(this::handle);
+                    graphics.setLayoutX(brandPosition);
+                    graphics.setLayoutY(positionY);
+                    nodes.add(graphics);
+                }/*else if(i==1){
+                    System.out.println("Du har valgt skjerm");
+                    Button screen = new Button("Velg skjerm");
+                    screen.setLayoutX(brandPosition);
+                    screen.setLayoutY(positionY);
+                    nodes.add(screen);
+                }
+               /* ChoiceBox<String> brandBox = new ChoiceBox<>();
                 brandBox.getItems().addAll("Merke", "Asus", "LG", "Samsung");
                 brandBox.setLayoutX(brandPosition);
                 brandBox.setLayoutY(positionY);
@@ -79,7 +127,7 @@ public class KundesideController implements Initializable{
                 sizeBox.setLayoutX(sizePosition);
                 sizeBox.setLayoutY(positionY);
                 sizeBox.setValue(sizeBox.getItems().get(0));
-                nodes.add(sizeBox);
+                nodes.add(sizeBox);*/
 
                 positionY += 50;
             }
@@ -90,13 +138,27 @@ public class KundesideController implements Initializable{
         innerPane.getChildren().addAll(nodes);
     }
 
+   /* public void choosenButton(int buttonPick){
+
+        graphics.setLayoutX(150);
+        removeGraphics.setLayoutX(300);
+        graphics.setLayoutY(63);
+        removeGraphics.setLayoutY(63);
+        innerPane.getChildren().addAll(graphics,removeGraphics);
+        graphics.setOnAction(event -> showChooser.pressedGraphics("Velg grafikkort",3));
+    }*/
+
     public void switchToMain() throws IOException {
         App.setRoot("main");
     }
+    public void switchToPreviousConfig() throws IOException{
+        App.setRoot("previousConfig");
+    }
+
 
 
     @FXML
-    Button back, choose, choose1;
+    Button back, choose, choose1, previousConf;
     @FXML
     ChoiceBox<String> brand, type, size;
     @FXML
---
+++ src/main/java/org/datavelger/showChooser.java
@@ -0,0 +1,50 @@
+package org.datavelger;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import org.datavelger.classes.Component;
+
+public class showChooser {
+    public static void pressedButton(String pressedButton){
+        System.out.println("Velger "+pressedButton+"...");
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(pressedButton);
+        Label label = new Label();
+        label.setText("Velg ønsket grafikkort");
+/*
+        TableView table = new TableView<>();
+        TableColumn name = new TableColumn("Navn");
+        TableColumn  series = new TableColumn("Serie");
+        TableColumn price = new TableColumn("Pris");
+        table.getColumns().addAll(name,series,price);
+        table.setOnMouseClicked((MouseEvent event)->{
+            //TODO: Sjekke hvilken entry som er klikket på i tableview
+        });
+
+    */    Label valgt = new Label();
+        valgt.setText("Du har valgt følgende grafikkort:");
+        //TODO: skrive ut valgt grafikkort basert på tableview
+
+        Button closeButton = new Button("Tilbake");
+        closeButton.setOnAction(e -> window.close());
+
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label,valgt,closeButton);
+        layout.setAlignment(Pos.CENTER);
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+    }
+}

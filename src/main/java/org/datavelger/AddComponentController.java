dff7cbf176620507fea65fd2c188fc45edf34653
Ole <olebastiank.lochen@gmail.com>
2020-05-13 15:30:06 +0200

    får lagret filer fra admin siden

--- src/main/java/org/datavelger/AddComponentController.java
+++ src/main/java/org/datavelger/AddComponentController.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import org.datavelger.Exceptions.InvalidNameException;
@@ -17,11 +16,9 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class AddComponentController implements Initializable {
-    private FileOpenerJSON fileOpenerJSON;
 
     @FXML
     private TableView<Component> table;
@@ -32,11 +29,11 @@ public class AddComponentController implements Initializable {
     @FXML
     private TextField txt_name, txt_price;
     @FXML
-    private Button btnAdd, btnDeleteComponent, btnDeleteOrder, btnCancel, btnComponentInformation;
+    private Button btnAdd, btnDeleteComponent, btnDeleteOrder, btnCancel, btnEditComp, btnSaveChanges;
     @FXML
     private Label labInfo;
     @FXML
-    private ChoiceBox <String> component;
+    private ChoiceBox <String> componentBox;
     @FXML
     private AnchorPane anchorPane;
 
@@ -44,14 +41,22 @@ public class AddComponentController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        try {
-            loadComponents();
-        } catch (IOException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
+        //last inn komponenter og table///////////////////////
+        loadComponents();
         collection.attachTableView(table);
 
+        //for å slette flere rader samtidig
+        table.getSelectionModel().setSelectionMode(
+                SelectionMode.MULTIPLE
+        );
+        //oppdatere tabell for å endre navn
+        namecol.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        pricecol.setCellValueFactory(new PropertyValueFactory<>("Price"));
+        namecol.editableProperty().setValue(true);
+        pricecol.editableProperty().setValue(true);
+
+
+        //initialisere knapper////////////////////////////////////////////
         btnAdd.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
             try {
                 AddRecord();
@@ -68,6 +73,18 @@ public class AddComponentController implements Initializable {
             }
         });
 
+        btnDeleteComponent.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            deleteComponent();
+        });
+
+        btnEditComp.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            editComponent();
+        });
+
+        btnSaveChanges.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            saveComponents();
+        });
+
         //vise ekstra info om komponent
         /*btnComponentInformation.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
             try {
@@ -78,51 +95,34 @@ public class AddComponentController implements Initializable {
         });
          */
 
-        //for å slette flere rader samtidig
-        table.getSelectionModel().setSelectionMode(
-                SelectionMode.MULTIPLE
-        );
-        //oppdatere tabell for å endre navn
-        table.setEditable(true);
-        //namecol.setCellFactory((TextFieldTableCell.forTableColumn()));
-        //pricecol.setCellFactory(((TextFieldTableCell.forTableColumn());
-
-
-        component.getItems().addAll(" ", "Grafikkort", "Harddisk", "Tastatur", "Minne", "Skjerm",
+        //gjør klar innlegging av komponenter //////////////////////////////////////////////
+        componentBox.getItems().addAll(" ", "Grafikkort", "Harddisk", "Tastatur", "Minne", "Skjerm",
                 "Hovedkort", "Mus", "Prosessor");
 
         //ikke mulig å gå videre uten å velge type komponent
-        component.getSelectionModel().selectedItemProperty().addListener((observableValue, oldChoice, newChoice) ->
-                btnAdd.setDisable(newChoice.equals(component.getItems().get(0))));
-        component.setValue(component.getItems().get(0));
-
-        namecol.setCellValueFactory(new PropertyValueFactory<>("Name"));
-        pricecol.setCellValueFactory(new PropertyValueFactory<>("Price"));
-
-        /*try {
-            table.setItems(getComponentList());
-        } catch (InvalidNameException | InvalidPriceException e) {
-            e.printStackTrace();
-        }*/
-
-        btnDeleteComponent.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
-            deleteButtonPushed();
-        });
-
+        componentBox.getSelectionModel().selectedItemProperty().addListener((observableValue, oldChoice, newChoice) ->
+                btnAdd.setDisable(newChoice.equals(componentBox.getItems().get(0))));
+        componentBox.setValue(componentBox.getItems().get(0));
     }
 
-    //slette rader som  er valgt i tabellen
-    public void deleteButtonPushed()
+    //slette rader som er valgt i tabellen
+    public void deleteComponent()
     {
-        ObservableList<Component> selectedRows, allcomponent;
-        allcomponent = table.getItems();
-
+        ObservableList<Component> selectedRows;
         selectedRows = table.getSelectionModel().getSelectedItems();
 
         for (Component component: selectedRows){
-            allcomponent.remove(component);
+            table.getItems().remove(component);
         }
     }
+
+    public void editComponent(){
+        Component editedComp = table.getSelectionModel().getSelectedItem();
+        txt_name.setText(editedComp.getName());
+        txt_price.setText(String.valueOf(editedComp.getPrice()));
+        table.getItems().remove(editedComp);
+    }
+
     public void changeNameCellEvent(TableColumn.CellEditEvent edditedCell) throws InvalidNameException {
         Component componentSelected = table.getSelectionModel().getSelectedItem();
         componentSelected.setName(edditedCell.getNewValue().toString());
@@ -142,13 +142,12 @@ public class AddComponentController implements Initializable {
 
         txt_name.clear();
         txt_price.clear();
-
     }
 
     //eksempel-komponenter
     public ObservableList<Component> getComponentList() throws InvalidNameException, InvalidPriceException {
         ObservableList<Component> components = FXCollections.observableArrayList();
-        components.add(new Component(  1299,"Keyboard") );
+        components.add(new Keyboard(1299,"Keyboard",false));
         components.add(new Component(1315,"Mouse") );
         components.add(new Component(299,"Motherboard") );
         components.add(new Component(1490,"Apple Magic Keyboard with Numeric Keypad") );
@@ -156,7 +155,7 @@ public class AddComponentController implements Initializable {
         return components;
     }
 
-    public void loadComponents() throws IOException, ClassNotFoundException {
+    public void loadComponents() {
         File folder = new File("components");
         FileOpenerBinary fileOpenerBinary = new FileOpenerBinary(folder.getPath());
         fileOpenerBinary.setOnSucceeded(event -> {
@@ -181,6 +180,25 @@ public class AddComponentController implements Initializable {
         thread.setDaemon(true);
         enableGUI(true);
         thread.start();
+    }
+
+    public void saveComponents(){
+        ArrayList<Component> components = new ArrayList<>(collection.getList());
+        for (Component component : components) {
+            String folder = "";
+            if(component.getClass().equals(GraphicsCard.class)) folder = "GraphicsCards";
+            if(component.getClass().equals(Harddrive.class)) folder = "Harddrives";
+            if(component.getClass().equals(Keyboard.class)) folder = "Keyboards";
+            if(component.getClass().equals(Memory.class)) folder = "Memory";
+            if(component.getClass().equals(Mouse.class)) folder = "Mice";
+            if(component.getClass().equals(Monitor.class)) folder = "Monitors";
+            if(component.getClass().equals(Motherboard.class)) folder = "Motherboards";
+            if(component.getClass().equals(Processor.class)) folder = "Processord";
+
+            FileSaverBinary fileSaverBinary = new FileSaverBinary(component);
+            fileSaverBinary.saveFile("components/" + folder
+                    + "/" + component.getName() + ".jobj");
+        }
     }
 
     public void enableGUI(boolean enable){

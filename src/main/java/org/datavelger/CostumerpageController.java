ac4d1f4802431845846051d912091ec6c8770183
Zandra Max <s333772@oslomet.no>
2020-05-15 16:54:02 +0200

    Ryddet litt i kommentarer og warnings

--- src/main/java/org/datavelger/CostumerpageController.java
+++ src/main/java/org/datavelger/CostumerpageController.java
@@ -120,83 +120,6 @@ public class CostumerpageController implements Initializable {
                 lblComp.setLayoutX(positionX);
                 lblComp.setDisable(true);
 
-                //ChoiceBox<Component>
-
-
-
-                /*Button add = new Button("Velg " + labelText.toLowerCase());
-                Button remove = new Button("Fjern valgt " + labelText.toLowerCase());
-                //String chosen = "Du har valgt følgende " + labelText.toLowerCase() + ": ";
-
-                nodes.addAll(Arrays.asList(lblComp, add, remove));
-                for (Node node : nodes){
-                    node.setLayoutY(positionY);
-                }
-                lblComp.setLayoutY(positionY);
-                add.setLayoutY(positionY);
-                remove.setLayoutY(positionY);
-
-                lblComp.setStyle("-fx-font-weight: bold");
-                add.setStyle("-fx-font-weight: bold");
-                remove.setStyle("-fx-font-weight: bold");
-
-                //TODO legge dette i css:
-
-                add.setPrefSize(150,30);
-                remove.setPrefSize(200, 30);
-                add.setLayoutX(150);
-                remove.setLayoutX(325);
-
-
-
-                //Kaller på metoden i showChooser med navnet på knappen som er valgt
-                String finalLabelText = labelText;
-                add.setOnAction(event -> {
-                    try {
-                        showChooser.pressedButton(finalLabelText);
-                        innerPane.getChildren().remove(btnChoose);
-                        lblComp.setDisable(false);
-                        lblComp.setText(components.get(components.size()-1));
-                        //innerPane.getChildren().addAll(chosencomp);
-                    } catch (IOException | ClassNotFoundException e) {
-                        e.printStackTrace();
-                    }
-                });
-                */
-
-                 /* System.out.println("Du har valgt grafikkort");
-                    Button graphics = new Button("Velg grafikkort");
-                    graphics.setOnAction(this::handle);
-                    graphics.setLayoutX(brandPosition);
-                    graphics.setLayoutY(positionY);
-                    nodes.add(graphics);
-                }/*else if(i==1){
-                    System.out.println("Du har valgt skjerm");
-                    Button screen = new Button("Velg skjerm");
-                    screen.setLayoutX(brandPosition);
-                    screen.setLayoutY(positionY);
-                    nodes.add(screen);
-                }
-               /* ChoiceBox<String> brandBox = new ChoiceBox<>();
-                brandBox.getItems().addAll("Merke", "Asus", "LG", "Samsung");
-                brandBox.setLayoutX(brandPosition);
-                brandBox.setLayoutY(positionY);
-                brandBox.setValue(brandBox.getItems().get(0));
-                nodes.add(brandBox);
-
-                ChoiceBox<String> typeBox = new ChoiceBox<>();
-                typeBox.getItems().addAll("Type", "LCD", "OLED");
-                typeBox.setLayoutX(typePosition);
-                typeBox.setLayoutY(positionY);
-                typeBox.setValue(typeBox.getItems().get(0));
-                nodes.add(typeBox);
-
-                ChoiceBox<String> sizeBox = new ChoiceBox<>();
-                sizeBox.getItems().addAll("Størrelse", "24\"", "27\"", "32\"");
-                sizeBox.setLayoutX(sizePosition);
-                sizeBox.setLayoutY(positionY);
-                sizeBox.setValue(sizeBox.getItems().get(0));
-                nodes.add(sizeBox);*/
 
                 positionY += 60;
             }
@@ -228,6 +151,4 @@ public class CostumerpageController implements Initializable {
     Pane innerPane;
     @FXML
     AnchorPane anchorPane;
-    @FXML
-    ScrollPane scrolling;
 }
